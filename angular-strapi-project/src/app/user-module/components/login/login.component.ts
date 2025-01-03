8acd68bcbb726643cd44daa51445a564394b2c51
Jan Kubiena <jan@kubiena.pl>
2025-01-03 19:33:48 +0100

    add login connection with Strapi and fix for-id attribute relation in registration html

--- angular-strapi-project/src/app/user-module/components/login/login.component.ts
+++ angular-strapi-project/src/app/user-module/components/login/login.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
+  imports: [FormsModule, CommonModule],
   standalone: true
 })
 export class LoginComponent {
+  formData: { identifier?: string; password?: string } = {};
+  message: string | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  login(event: Event): void {
+    event.preventDefault();
+    this.message = null;
+
+    this.http
+      .post('http://127.0.0.1:8082/api/auth/local', this.formData)
+      .subscribe({
+        next: (res: any) => {
+          if (res.error) {
+            this.message = res.error.message;
+          } else if (res.jwt && res.user) {
+            this.message = 'Logowanie zakończone sukcesem.';
+          }
+        },
+        error: (err) => {
+          this.message = 'Nieprawidłowe dane logowania. Spróbuj ponownie.';
+        }
+      });
+  }
 }
