7e5930f7179b0761838ee477459bda53248f970b
Jan Kubiena <jan@kubiena.pl>
2025-01-11 15:09:22 +0100

    add user model and localStorage hanlding service, completly re-shaped authentication service

--- angular-strapi-project/src/app/services/authentication.service.ts
+++ angular-strapi-project/src/app/services/authentication.service.ts
@@ -1,28 +1,68 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../models/user';
+import { StorageService } from './storage.service';
+
+interface AuthResponse {
+  jwt: string;
+  user: User;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticatedService {
-  private apiUrl = 'http://127.0.0.1:8082/api/auth/local';
+  private starpiUrl = 'http://127.0.0.1:8082/api/auth/local';
+  private loginTracker = new BehaviorSubject(this.checkIfLoggedIn());
+    
+  loggedInStatus$ = this.loginTracker.asObservable();
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private ss: StorageService) {}
 
-  login(formData: { identifier: string; password: string }): Observable<any> {
-    return this.http.post(this.apiUrl, formData).pipe(
-      tap((res: any) => {
-        if (res.jwt) {
-          localStorage.setItem('jwt', res.jwt);
-        }
-      })
-    );
+  login(identifier: string, password: string) {
+    return this.http.post<AuthResponse>(this.starpiUrl, { identifier, password });
   }
 
-  isAuthenticated(): boolean {
-    return !!localStorage.getItem('jwt');
+  register(username: string, email: string, password: string) {
+    return this.http.post<AuthResponse>(`${this.starpiUrl}/register`, { username, email, password });
   }
+
+  checkIfLoggedIn() {
+    return this.ss.getItem('loggedIn') === 'true';
+  }
+
+  persistUser(resp: AuthResponse) {
+    [
+      ['userId', resp.user.id],
+      ['userEmail', resp.user.email],
+      ['username', resp.user.username],
+      ['loggedIn', 'true'],
+      ['token', resp.jwt]
+    ].forEach(item => this.ss.setItem(item[0], item[1]));
+    this.loginTracker.next(true);
+  }
+
+  getPersistedUser(): User {
+    return {
+      id: this.ss.getItem('userId') || '',
+      username: this.ss.getItem('username') || '',
+      email: this.ss.getItem('userEmail') || ''
+    };
+  }
+
+  getPersistedToken(): string {
+    return this.ss.getItem('token') || '';
+  }
+
+  logout() {
+    ['userId', 'userEmail', 'username', 'loggedIn', 'token'].forEach(item => this.ss.removeItem(item));
+    this.loginTracker.next(false);
+  }
+
+  getAuthHeader() {
+    return {
+      headers: { 'Authorization': `Bearer ${this.getPersistedToken()}` }
+    };
+  };
 }
