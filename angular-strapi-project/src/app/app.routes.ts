d539d721bcbf429742fca366458aa4ed5244467d
Jan Kubiena <jan@kubiena.pl>
2025-01-26 16:38:39 +0100

    add home page with button that directs to films-list

--- angular-strapi-project/src/app/app.routes.ts
+++ angular-strapi-project/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {path: 'user', loadChildren: () => import('./user-module/user-module.module').then((m) => m.UserModuleModule)},
     {path: 'films', loadChildren: () => import('./film-module/film-module.module').then((m) => m.FilmModuleModule)},
+    {path: '', component: HomeComponent}
 ];
